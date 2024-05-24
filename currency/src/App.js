5f6d3ea76d5f78b37b0a79b45acdd4ab7bf3bf21
mulundapm <mulundapm@yahoo.com.hk>
2024-05-25 00:16:16 +0200

    learning how to handle api key

--- currency/src/App.js
+++ currency/src/App.js
@@ -5,31 +5,30 @@ import { useState , useEffect} from 'react';
 import axios from 'axios';
 
 function App() {
-
   const [amount1, setAmount1] = useState(1)
   const [amount2, setAmount2] = useState(1)
   const [currency1, setCurrency1] = useState('USD')
   const [currency2, setCurrency2] = useState('EUR')
   const [rates, setRate] = useState('')
-
+  
   useEffect (() =>{
-    axios.get('https://api.currencybeacon.com/v1/latest?api_key=wPMvk6TXDXOnqEKs8rJH0khNZfiBc3P1')
+    axios.get('https://api.currencybeacon.com/v1/latest?api_key='+process.env.REACT_APP_API_KEY)
     .then(response => {
       setRate(response.data.rates)
     })
   }, []);
-
+  
   useEffect ( ()=>{
     if (!!rates){
       handleAmount1Change(1)
     }
   },[rates])
-
-
+  
+  
   function format(number){
     return number.toFixed(4)
   }
-
+  
   function handleAmount1Change(amount1){
     setAmount2(format(amount1 * rates[currency2] /rates[currency1]))
     setAmount1(amount1);
@@ -44,12 +43,13 @@ function App() {
     setAmount1(format(amount2 * rates[currency1] /rates[currency2]))
     setAmount2(amount2);
   }
-  
+
   function handleCurrency2Change(currency2){
     setCurrency2(currency2);
     setAmount1(format(amount2 * rates[currency1] /rates[currency2]))
   }
   
+
   return(  
   <div>
     <h1>Currency Converter</h1>
